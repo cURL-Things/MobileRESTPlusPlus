cd52e9e95ad2dbf386b90cf6f86231a21b4cafa5
Ryan Sander <ryansander.chicago@gmail.com>
2013-05-31 13:35:55 -0500

    Added method to get response code from service path and did example implementation in test service project

--- MobileRESTPlusPlus.cpp
+++ MobileRESTPlusPlus.cpp
@@ -7,3 +7,30 @@
 //
 
 #include "MobileRESTPlusPlus.h"
+
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+    return NULL;
+}
+
+int statusCodeForServicePath(std::string servicePath) {
+    
+    CURL *curl;
+    CURLcode curl_code;
+    
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, servicePath.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_code = curl_easy_perform(curl);
+    
+    int http_status_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_status_code);
+    
+    return http_status_code;
+    
+}
+
+void printSucka() {
+    
+    printf("ERC!!");
+    
+}
